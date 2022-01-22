b787ebab0341af46f63d09131541d6255fc79e10
aldooo05 <mardotillahdarmawan29@gmail.com>
2022-01-22 11:38:47 +0700

    Belajar flutter
    menambahkan collom
    menambahkan row
    menambahkan widget

---
+++ lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.green,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("HALAMAN PERTAMA"),
+          actions: [IconButton(onPressed: () {}, icon: Icon(Icons.menu))],
+        ),
+        body: Center(
+            child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Column(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+              Text("Text 1"),
+              Text("Text 2"),
+              Text("Text 3"),
+            ]),
+            Column(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+              Text("Text 4"),
+              Text("Text 5"),
+              Text("Text 6"),
+            ]),
+            Column(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+              Text("Text 7"),
+              Text("Text 8"),
+              Text("Text 9"),
+            ]),
+          ],
+        )));
+  }
+}
